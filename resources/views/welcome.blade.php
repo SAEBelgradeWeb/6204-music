e7596a45ea2b1444310c9f4867691e2a9213e9b9
Your Name <you@example.com>
2019-04-12 17:52:07 +0200

    kasjd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    A chabnge
+                    A new thing
                     <input type="text" id="test">
                     <button id="ok">Ok</button>
                 </div>
